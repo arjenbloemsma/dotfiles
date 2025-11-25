5f39e24d50c60270396ded366ec452cd576cf939
Arjen Bloemsma <arjen@bloemium.com>
2025-11-25 19:09:08 +0100

    Add markdown-preview Firefox profile config

---
+++ nvim/.config/nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,12 @@
+return {
+  "iamcco/markdown-preview.nvim",
+  config = function()
+    vim.g.mkdp_browserfunc = "OpenMarkdownPreview"
+    vim.cmd([[
+      function! OpenMarkdownPreview(url)
+        execute 'silent !/Applications/Firefox.app/Contents/MacOS/firefox --profile "/Users/arjenbloemsma/Library/Application Support/Firefox/Profiles/F0KdqyUG.Profile 3" ' . shellescape(a:url) . ' &'
+      endfunction
+    ]])
+    vim.cmd([[do FileType]])
+  end,
+}
